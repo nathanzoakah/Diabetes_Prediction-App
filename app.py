68e949ff0616b6d75486aa1cc54b47a398c47861
nathanzoakah <45339797+nathanzoakah@users.noreply.github.com>
2023-06-19 19:40:48 +0100

    Add files via upload
    Addition of extra changes to the menu

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 from predict import show_predict_page
+from explore import show_explore_page
+from glucose import show_glucose_page
 
-st.sidebar.selectbox("MENU", ("Diabetes Prediction", "Exploratory Data Analysis", "Blood Glucose Level Prediction"))
+page = st.sidebar.selectbox("MENU", ("Diabetes Prediction", "Exploratory Data Analysis", "Blood Glucose Level Prediction"))
 
-show_predict_page()
+if page == "Diabetes Prediction":
+    show_predict_page()
+elif page == "Exploratory Data Analysis": 
+    show_explore_page()
+elif page =="Blood Glucose Level Prediction":
+    show_glucose_page()
---
+++ glucose.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def show_glucose_page():
+    st.write("""## COMING SOON!!""")
