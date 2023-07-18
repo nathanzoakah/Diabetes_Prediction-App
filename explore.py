867772260934bc2dbfdc4f118194ad146fb3b93f
nathanzoakah <45339797+nathanzoakah@users.noreply.github.com>
2023-07-18 14:53:51 +0100

    Update explore.py

--- explore.py
+++ explore.py
@@ -14,43 +14,42 @@ def show_explore_page():
     st.write("""## COMING SOON!!""")
     st.write("""### On this page we'll explore the dataset used to train our machine learning models""")
 
-    """
-    data = load_data()
+    
+    #data = load_data()
 
-    st.write("""#### Top five rows of the diabetes Dataset""")
-    st.dataframe(data.head())
+    #st.write("""#### Top five rows of the diabetes Dataset""")
+    #st.dataframe(data.head())
 
-    st.write("""#### Bottom five rows of the diabetes Dataset""")
-    st.dataframe(data.tail())
+    #st.write("""#### Bottom five rows of the diabetes Dataset""")
+    #st.dataframe(data.tail())
 
 
-    st.write("""#### Description of the diabetes Dataset""")
-    st.dataframe(data.describe())
+    #st.write("""#### Description of the diabetes Dataset""")
+    #st.dataframe(data.describe())
 
 
     
 
     #st.dataframe(data["Diagnosis"].value_counts())
 
-    df = data["Diagnosis"].value_counts()
+    #df = data["Diagnosis"].value_counts()
 
-    fig1, ax1 = plt.subplots()
-    ax1.pie(df, labels = ['No Diabetes', 'Diabetes'], autopct="%1.1f%%", shadow=True, startangle=90)
-    ax1.axis("equal")
+    #fig1, ax1 = plt.subplots()
+    #ax1.pie(df, labels = ['No Diabetes', 'Diabetes'], autopct="%1.1f%%", shadow=True, startangle=90)
+    #ax1.axis("equal")
 
-    st.write("""#### Percentage of Target Variable""")
-    st.pyplot(fig1)
+    #st.write("""#### Percentage of Target Variable""")
+    #st.pyplot(fig1)
 
-    df2 = data["Gender_M"].value_counts()
+    #df2 = data["Gender_M"].value_counts()
 
-    fig2, ax2 = plt.subplots()
-    ax2.pie(df2, labels = ['Female','Male'], autopct="%1.1f%%", shadow=True, startangle=90)
-    ax2.axis("equal")
+    #fig2, ax2 = plt.subplots()
+    #ax2.pie(df2, labels = ['Female','Male'], autopct="%1.1f%%", shadow=True, startangle=90)
+    #ax2.axis("equal")
     
 
-    st.write("""#### Percentage of Gender""")
-    st.pyplot(fig2)
-    """
+    #st.write("""#### Percentage of Gender""")
+    #st.pyplot(fig2)
 
 
 
